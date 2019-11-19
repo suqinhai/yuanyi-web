e8ec4297ecd0ba392a3b643b2d4ec312f516b1ae
suqinhai <467456744@qq.com>
2019-11-20 00:22:36 +0800

    修改

--- assets/js/common.js
+++ assets/js/common.js
@@ -58,3 +58,18 @@ function ajaxPost(url, data, fn) {
         }
     })
 }
+
+
+
+/**
+ * 获取浏览器参数
+ */
+function getQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return unescape(r[2]);
+    } else {
+        return null;
+    }
+}
